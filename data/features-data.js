1e238a5a57373c7d9b82f32b49987787817b835b
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-01-18 17:00:01 -0500

    feat: responsive nav (WIP)

---
+++ data/features-data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        image: "/images/illustration-features-tab-1.svg",
+        alt: "dark themed illustrated computer screen",
+        heading: "Bookmark in one click",
+        text:"Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favorite sites."
+    },
+    {
+        image: "/images/illustration-features-tab-2.svg" ,
+        alt: "illustrated computer screen with magnifying glass inspecting elements on page",
+        heading: "Intelligent search",
+        text: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
+    },
+    {
+        image: "/images/illustration-features-tab-3.svg",
+        alt: "illustrated women inside a screen high-fiving a man standing outside of the screen",
+        heading: "Share your bookmarks",
+        text: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."
+    },
+]
--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-// onclick to hide nav for mobile
-
+// import data from './data/features-data'
 // add class function - will be reused.
-// remove call function - will be reused.
+const addClass = (element, classToAdd) => {
+    if(!element.classList.contains(classToAdd)) {
+        element.classList.add(classToAdd)
+    } else {
+        return
+    }
+}
+// remove class function - will be reused.
+const removeClass = (element, classToRemove) => {
+    if(element.classList.contains(classToRemove)) {
+        element.classList.remove(classToRemove)
+    } else {
+        return
+    }
+}
+
+// onclick to hide nav for mobile- change nav-btn innerHTMl to show the close nav btn / toggle it. add class hide when close btn is clicked
+
+const navBtn = document.getElementById('nav-btn')
+navBtn.addEventListener('click', () => {
+    const navItems = document.getElementById('nav--items')
+    const logo = document.getElementById('header--logo-container')
+    if(navItems.classList.contains('hide')) {
+        console.log(navItems)
+        removeClass(navItems, 'hide')
+        addClass(navItems,'show')
+        navBtn.innerHTML = `<img src="/images/icon-close.svg" alt="close navigation button" id="close-nav"/>`
+        logo.innerHTML = `<img src="/images/logo-light.png" alt="bookmark logo" class="header--img_logo"/>`
+    } else {
+        addClass(navItems, 'hide')
+        removeClass(navItems, 'show')
+        navBtn.innerHTML = `<img src="/images/icon-hamburger.svg" alt="open navigation button" id="open-nav"/>`
+        logo.innerHTML = `<img src="/images/logo-bookmark.svg" alt="bookmark logo" class="header--img_logo"/>`
+    }
+})
+
 // array of objs- Simple Bookmarking, Speedy Searching, and Easy Sharing info
 
 // onclick of one of the tabs, selects correct obj, changes inner text, and adds class to underline selected and remove selected class from non selected tabs. ->
